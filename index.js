9978de5989cee0a2dbcd324e32783fa207318564
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-03-02 18:17:00 -0500

    commit tree 7ac33d60d2a6a7e8c83b8bef69ff3c4e2b9883cf

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
-const fi = (function() {
+const fi = (function () {
   return {
-    libraryMethod: function() {
+    libraryMethod: function () {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function (coll, func) {
+      for (let i = 0; i < coll.length; i++) {
+        func(coll[i])
+      }
+      return coll
+    },
+
+    map: function (coll, func) {
+      let result = []
+      for (let i = 0; i < coll.length; i++) {
+        result[i] = func(coll[i])
+      }
+      return result
+    },
+
+    reduce: function () {
 
     },
 
-    map: function() {
+    find: function (coll, pred) {
+      for (let i = 0; i < coll.length; i++) {
+        if (pred(coll[i])) {
+          return coll[i]
+        }
+      }
+      return undefined
+    },
+
+    filter: function (coll, pred) {
+      let result = []
+      for (let i = 0; i < coll.length; i++) {
+        if (pred(coll[i])) {
+          result.push(coll[i])
+        }
+      }
+      return result
+    },
+
+    functions: function () {
 
     },
 
-    reduce: function() {
-
-    },
-
-    functions: function() {
-
-    },
-
+    size: function (coll) {
+      let num = 0;
+      coll.forEach(el => num++)
+      return num
+    }
 
   }
 })()
